456b17f32f9442e56d83d6feb07d8c2a8cd29709
snowlaxc(mac_ubuntu) <snowlaxc@gmail.com>
2023-12-20 15:16:43 +0000

    이메일 확인 오류 해결 및 주석 추가

--- frontend_npm/src/views/loginPage.js
+++ frontend_npm/src/views/loginPage.js
@@ -1,12 +1,17 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import { useContext } from "react"; // 리액트에서 useContext 모듈을 가져옵니다.
+import AuthContext from "../context/AuthContext"; // 커스텀 인증 컨텍스트를 가져옵니다.
 
 const LoginPage = () => {
-  const { loginUser } = useContext(AuthContext);
+  const { loginUser } = useContext(AuthContext); // AuthContext에서 loginUser 함수를 가져옵니다.
+
   const handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault(); // 폼 제출 기본 동작을 막습니다. 페이지 리로드를 방지합니다.
+
+    // 폼에서 사용자명(username)과 비밀번호(password)를 가져옵니다.
     const username = e.target.username.value;
     const password = e.target.password.value;
+
+    // 사용자명이 비어 있지 않은 경우에만 loginUser 함수를 호출하여 로그인을 시도합니다.
     username.length > 0 && loginUser(username, password);
   };
 
@@ -21,7 +26,7 @@ const LoginPage = () => {
         <label htmlFor="password">Password</label>
         <input type="password" id="password" placeholder="Enter Password" />
         <br/>
-        <button type="submit">Login</button>
+        <button type="submit">Login</button> {/* 폼 제출을 위한 로그인 버튼 */}
       </form>
     </section>
   );
