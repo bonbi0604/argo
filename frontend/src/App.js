4445335fb19e23062f888594029ce734d01a034c
woongyu1221 <dldnsrb1221@naver.com>
2023-12-26 14:28:12 +0900

    app.js 새미콜론 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 // App.js
-import React from "react"
-import "./index.css"
-import "./App.css"
-import Footer from "./components/Footer"
-import HeaderMenu from "./components/HeaderMenu"
-import { BrowserRouter as Router,  Routes,  Route} from "react-router-dom"
-// import PrivateRoute from "./utils/PrivateRoute"
-import { AuthProvider } from "./context/AuthContext"
-import Home from "./views/homePage"
-import Login from "./views/loginPage"
-import Register from "./views/registerPage"
-import ProtectedPage from "./views/protectedPage"
-import FindId from "./views/find/findIdPage"
-import FindPw from "./views/find/findPwPage"
-import Dashboard from "./views/Dashboard"
-import WritePost from './views/WritePost'
-import Chatbot from "./components/Chatbot"
-import Chat from "./views/chat"
-import CommunicationPage from "./views/CommunicationPage"
-import LearningPage from "./views/learningPage"
-import EthicPage from "./views/ethicPage"
+import React, { useState } from "react";
+import "./index.css";
+import "./App.css";
+import Footer from "./components/Footer";
+import HeaderMenu from "./components/HeaderMenu";
+import { BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+// import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/protectedPage";
+import FindId from "./views/find/findIdPage";
+import FindPw from "./views/find/findPwPage";
+import Dashboard from "./views/Dashboard";
+import WritePost from './views/WritePost';
+import Chatbot from "./components/Chatbot";
+import Chat from "./views/chat";
+import CommunicationPage from "./views/CommunicationPage";
+import LearningPage from "./views/learningPage";
+import EthicPage from "./views/ethicPage";
 
 function App() {
   return (
