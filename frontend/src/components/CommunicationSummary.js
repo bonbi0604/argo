f9ab21c2920701ea9e4238a726d8830bc2e3edff
BaeJihyun97 <baejihyun970519@gmail.com>
2023-12-28 23:35:04 +0900

    radar chart

--- frontend/src/components/CommunicationSummary.js
+++ frontend/src/components/CommunicationSummary.js
@@ -1,11 +1,20 @@
+import React, { useContext } from 'react';
 import "./CommunicationSummary.css"
 import RadarChart from "./RadarChart"
+import AuthContext from "../context/AuthContext";
+
+
 
 const CommunicationSummary = () => {
+  const { user } = useContext(AuthContext);
+
+  const dataUser = [23, 43, 62, 15, 84, 34, 46];
+  const dataAvg = [64, 43, 34, 24, 56, 32, 64];
+  
   return (
     <div className="radar_chart_wrapper">
       <div className="radar_chart">
-        <RadarChart />
+        <RadarChart dataUser={dataUser} dataAvg={dataAvg} name={user.name} />
       </div>
         
     </div>
--- frontend/src/components/RadarChart.js
+++ frontend/src/components/RadarChart.js
@@ -19,30 +19,30 @@ ChartJS.register(
   Legend
 );
 
-const RadarChart = () => {
+const RadarChart = ({dataUser, dataAvg, name}) => {
   
   const data = {
-    labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+    labels: ['Clear', 'Concise', 'Concrete', 'Correct', 'Coherent', 'Complete', 'Courteous'],
     datasets: [
         {
-          label: 'Dataset 1',
-          data: [65, 59, 90, 81, 56, 55, 40],
+          label: `${name} 님`,
+          data: dataUser,
+          backgroundColor: 'rgba(242, 173, 0, 0.2)', // #FAE3A9
+          borderColor: 'rgba(242, 173, 0, 1)',
+          pointBackgroundColor: 'rgba(236, 206, 131, 1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(236, 206, 131, 1)',
+        },
+        {
+          label: '평균',
+          data: dataAvg,
           backgroundColor: 'rgba(179, 181, 198, 0.2)',
           borderColor: 'rgba(179, 181, 198, 1)',
           pointBackgroundColor: 'rgba(179, 181, 198, 1)',
           pointBorderColor: '#fff',
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgba(179, 181, 198, 1)',
-        },
-        {
-          label: 'Dataset 2',
-          data: [50, 69, 70, 80, 76, 45, 49],
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          pointBackgroundColor: 'rgba(255, 99, 132, 1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
         },
       ],
     };
