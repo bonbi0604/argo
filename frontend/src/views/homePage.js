2adb0d27a4692e2c725bf5d90a6be3ba28094ab6
woongyu1221 <dldnsrb1221@naver.com>
2024-01-05 23:34:57 +0900

    메인페이지 꾸미는중

--- frontend/src/views/homePage.js
+++ frontend/src/views/homePage.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 console.log("homepage"); // "homepage"을 콘솔에 출력합니다.
 
 const Home = () => {
+  
   useEffect(() => {
+    
     const loadScript = src => {
       return new Promise((resolve, reject) => {
         const script = document.createElement('script');
@@ -42,11 +44,11 @@ const Home = () => {
         <section id="banner" className="major">
           <div className="inner">
             <header className="major">
-              <h1>Hi, my name is Forty</h1>
+              <h1>환영합니다~</h1>
             </header>
             <div className="content">
-              <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+              <p>신입사원분들의 역량 개발을 도와드립니다.<br />
+                사내규정을 챗봇에게 물어보세요.</p>
               <ul className="actions">
                 <li><a href="#one" className="button next scrolly">Get Started</a></li>
               </ul>
@@ -62,7 +64,7 @@ const Home = () => {
                 <img src="../../home/images/pic01.jpg" alt />
               </span>
               <header className="major">
-                <h3><a href="../../home/landing.html" className="link">Aliquam</a></h3>
+                <h3><a href="../../home/landing.html" className="link">피드백</a></h3>
                 <p>Ipsum dolor sit amet</p>
               </header>
             </article>
@@ -71,7 +73,7 @@ const Home = () => {
                 <img src="../../home/images/pic02.jpg" alt />
               </span>
               <header className="major">
-                <h3><a href="../../home/landing.html" className="link">Tempus</a></h3>
+                <h3><a href="../../home/landing.html" className="link">역량 개발</a></h3>
                 <p>feugiat amet tempus</p>
               </header>
             </article>
@@ -80,7 +82,7 @@ const Home = () => {
                 <img src="../../home/images/pic03.jpg" alt />
               </span>
               <header className="major">
-                <h3><a href="../../home/landing.html" className="link">Magna</a></h3>
+                <h3><a href="../../home/landing.html" className="link">사내규정 도우미</a></h3>
                 <p>Lorem etiam nullam</p>
               </header>
             </article>
@@ -89,11 +91,11 @@ const Home = () => {
                 <img src="../../home/images/pic04.jpg" alt />
               </span>
               <header className="major">
-                <h3><a href="../../home/landing.html" className="link">Ipsum</a></h3>
+                <h3><a href="../../home/landing.html" className="link">게시판</a></h3>
                 <p>Nisl sed aliquam</p>
               </header>
             </article>
-            <article>
+            {/* <article>
               <span className="image">
                 <img src="../../home/images/pic05.jpg" alt />
               </span>
@@ -110,7 +112,7 @@ const Home = () => {
                 <h3><a href="../../home/landing.html" className="link">Etiam</a></h3>
                 <p>Feugiat amet tempus</p>
               </header>
-            </article>
+            </article> */}
           </section>
           {/* Two */}
           <section id="two">
