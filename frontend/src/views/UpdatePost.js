7adeb165bbbbab9a4cfbb7450b340356ce8fb136
hong <wendy1009@naver.com>
2024-01-15 10:48:47 +0900

    import css 추가

--- frontend/src/views/UpdatePost.js
+++ frontend/src/views/UpdatePost.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useAxios from "../utils/useAxios";
+import "./WritePost.css"
 import "./UpdatePost.css";
 
 const UpdatePost = () => {
